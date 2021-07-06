ab3efb52e8b8850771c1fd19d767d788ec3cae03
d2525456d <72094644+d2525456d@users.noreply.github.com>
2021-07-06 22:00:19 +0700

    BT12

--- CTDLGT_R/BT12_CaiDatDoThiVoHuong.R
+++ CTDLGT_R/BT12_CaiDatDoThiVoHuong.R
@@ -1,4 +1,4 @@
 library(igraph)
 
-vh = make_graph(c(1, 2, 2, 3, 3, 4, 5, 6, 4,1, 2,4, 4,5, 6,1), directed = FALSE)
+vh = make_graph(c(1, 3, 6, 3, 3, 1, 5, 6, 4,1, 2,4, 4,5, 6,1), directed = FALSE)
 plot(vh)
