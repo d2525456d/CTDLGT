c403b1f3fdd1b4320f2d63813aebbee4cc94a432
d2525456d <72094644+d2525456d@users.noreply.github.com>
2021-07-19 07:53:36 +0700

    BT01

--- CTDLGT_Python/BT01_ThapHaNoi.py
+++ CTDLGT_Python/BT01_ThapHaNoi.py
@@ -1,9 +1,11 @@
+#Nhập số dĩa
 n = int(input('nhap so dia:'))
+#tạo 1 def
 def thaphanoi(n, cot_goc, cot_cuoi, trung_gian):
   if n == 1:
-    print("Di chuyển dĩa 1 từ cột",cot_goc,"đến cột",cot_cuoi)
+    print("Di chuyển dĩa 1 từ cột",cot_goc,"đến cột",cot_cuoi) 
     return
-  thaphanoi(n-1, cot_goc, trung_gian, cot_cuoi)
+  thaphanoi(n-1, cot_goc, trung_gian, cot_cuoi) # sử dụng đệ quy đổi chỗ
   print ("Di chuyển dĩa ",n,"từ cột",cot_goc,"đến cột",cot_cuoi)
-  thaphanoi(n-1, trung_gian, cot_cuoi, cot_goc)
-print(thaphanoi(n,"gốc","cuối","trung gian"))
+  thaphanoi(n-1, trung_gian, cot_cuoi, cot_goc) #sử dụng đệ quy đổi chỗ
+print(thaphanoi(n,"gốc","cuối","trung gian")) # in ra để chạy
